ad5dabafd21107b3f20afa02316df70da64187c7
OfficialGopi <official.gopi@outlook.com>
2025-10-29 22:13:05 +0530

    fix: add new file to gitignore

--- web/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./router/Router";
-
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>
-);
